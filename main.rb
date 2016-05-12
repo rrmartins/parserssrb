733e95ffaf6c854bbfe995fb2a9578d71fed002b
Rodrigo Martins <rrmartinsjg@gmail.com>
2016-05-12 20:54:45 -0300

    add benchmark

--- main.rb
+++ main.rb
@@ -1,16 +1,19 @@
 require 'rubygems'
 require './parse/rss'
+require 'benchmark'
 
-url = "http://golangweekly.com/rss/1g2bo910"
-parserss = Parse::Rss.new(url)
+puts Benchmark.measure {
+  url = "http://golangweekly.com/rss/1g2bo910"
+  parserss = Parse::Rss.new(url)
 
-puts("The Title is: #{parserss.get_title}\n")
-puts("The Description is: #{parserss.get_description}\n")
-puts("The Link is: #{parserss.get_link}\n")
+  puts("The Title is: #{parserss.get_title}\n")
+  puts("The Description is: #{parserss.get_description}\n")
+  puts("The Link is: #{parserss.get_link}\n")
 
-categories = parserss.get_categories
-categories.each do |category|
-  puts("The Categories is #{category}\n")
-end
+  categories = parserss.get_categories
+  categories.each do |category|
+    puts("The Categories is #{category}\n")
+  end
 
-puts("The total of Items is: #{parserss.get_total_items}\n\n")
+  puts("The total of Items is: #{parserss.get_total_items}\n\n")
+}
